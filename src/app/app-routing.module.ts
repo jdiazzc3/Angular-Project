f833d4db2f7d0636bb6f2128d319b57cf49fc070
jdiazzc3 <jddiazz.036@gmail.com>
2023-04-08 19:14:02 -0500

    Agrego frontend de todo le auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { VerifyComponent } from './components/verify/verify.component';
+import { ForgotPassComponent } from './components/forgot-pass/forgot-pass.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo:'login', pathMatch:'full' },
+  { path:'login', component:LoginComponent },
+  { path:"register", component:RegisterComponent },
+  { path:"verify", component:VerifyComponent },
+  { path:"forgot-password", component:ForgotPassComponent },
+  { path:"dashboard", component:DashboardComponent },
+  { path:"*/", redirectTo:'login', pathMatch:"full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { VerifyComponent } from './components/verify/verify.component';
+import { ForgotPassComponent } from './components/forgot-pass/forgot-pass.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    VerifyComponent,
+    ForgotPassComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
-  ],
+   ],
   providers: [],
   bootstrap: [AppComponent]
 })
